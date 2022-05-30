5d9fa40ae576cdbba89a5c08258f3dc838a2dc81
maliyev2 <maliyev2@stu.vistula.edu.pl>
2022-05-30 15:42:56 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        NameList.Start();
+    }
+
+    }
+
